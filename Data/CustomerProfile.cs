bca1ea2a937c04b68e97f50127176d567b77ea9d
OdiZgb <84592093+OdiZgb@users.noreply.github.com>
2023-06-27 03:58:17 +0300

    Delete unneeded  automapper mappings

--- Data/CustomerProfile.cs
+++ Data/CustomerProfile.cs
@@ -5,8 +5,6 @@ public class CustomerProfile : Profile
     public CustomerProfile()
     {
         CreateMap<Trader, TraderDTO>();
-        CreateMap<List<Trader>, List<TraderDTO>>();
         CreateMap<TraderDTO, Trader>();
-        CreateMap<List<TraderDTO>, List<Trader>>();
     }
 }
